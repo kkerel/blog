0ea0540f3e764896364716ca6aa5891f5b1d9d5f
kkerel <junesu_1104@naver.com>
2018-07-20 18:34:24 +0900

    blog adjust

---
+++ frontend/src/components/layout/Aside.vue
@@ -0,0 +1,51 @@
+<template>
+	<aside>
+		<h1>June Su<!-- June Blog&nbsp;★ --></h1>
+		<nav>
+			<ul>
+				<li>
+					<router-link v-bind:to="{ name: 'Home' }">
+						<span class="title">홈</span>
+					</router-link>
+				</li>
+				<li>
+					<router-link v-bind:to="{ name: 'Profile' }">
+						<span class="title">프로필</span>
+					</router-link>
+				</li>
+				<li>
+					<router-link v-bind:to="{ name: 'Referencedata' }">
+						<span class="title">자료</span>
+					</router-link>
+				</li>
+			</ul>
+		</nav>
+	</aside>
+</template>
+
+<script>
+
+export default {
+	name: 'Aside',
+	components: {},
+	data () {
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+$main-color:#323c48;
+$basic-color:#fff;
+h1{border-bottom: 1px solid #757e89; height: 60px; line-height: 60px; text-align: center; font-size: 18px;}
+aside{ position: fixed; top: 0px; left: 0px; z-index: 10; width: 250px; height: 100%; background: $main-color; color:$basic-color;
+	ul{
+		li{
+			a{height:60px; line-height:60px; display:block; padding-left:45px; color:#757e89; border-bottom:1px solid #757e89; font-size:12px; letter-spacing:1px; background:url('../../assets/image/icon/aside_off_i.png') 20px center no-repeat;
+				&:hover{color:$basic-color; background:#2b343f url('../../assets/image/icon/aside_on_i.png') 20px center no-repeat;}
+			}
+			.router-link-exact-active{color:$basic-color; background:#2b343f url('../../assets/image/icon/aside_on_i.png') 20px center no-repeat; border-right:5px solid #00b3a5;}
+		}
+	}
+}
+</style>
---
+++ frontend/src/components/layout/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <div class="button">
+      <div class="button_cover"></div>
+      <a href="https://github.com/kkerel" target="_blank">GIT</a>
+    </div>
+    <div class="button">
+      <div class="button_cover"></div>
+      <a href="http://junesu1104.dothome.co.kr/" target="_blank">포트폴리오</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  footer{ position:fixed; bottom:0px; left:0px; width: 100%; z-index: 1; padding:10px 0px 10px 250px; background:#323c48;
+    .button{ width:140px; height:50px; border:2px solid #fff; float:left; text-align:center; cursor:pointer; position:relative; box-sizing:border-box; overflow:hidden; background:#fff; margin-right:10px; background:#323c48;
+      .button_cover{width:140px; height:50px; top:-50px; background:#fff; position:absolute; transition:all .5s ease; z-index:1;}
+      a{font-size:16px; color:#fff; text-decoration:none; line-height:50px; transition:all .5s ease; z-index:2; position:relative; display:block; font-weight:bold;}
+      &:hover{
+        a{color:#34495e;}
+        .button_cover{top:0;}
+      }
+    }
+  }
+</style>
